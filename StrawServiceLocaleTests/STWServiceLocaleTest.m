6d8d550a896b29f59d7b1fd732f43e48d803ea9e
Yoshiya Hinosawa <stibium121@gmail.com>
2014-09-17 22:21:09 +0100

    Add test of getLanguage

--- StrawServiceLocaleTests/STWServiceLocaleTest.m
+++ StrawServiceLocaleTests/STWServiceLocaleTest.m
@@ -1,4 +1,12 @@
 #import <XCTest/XCTest.h>
+#import "STWServiceLocale.h"
+
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#define MOCKITO_SHORTHAND
+#import <OCMockito/OCMockito.h>
+
 
 @interface STWServiceLocaleTest : XCTestCase
 
@@ -7,9 +15,29 @@
 @implementation STWServiceLocaleTest
 
 
-- (void)testExample
+- (void)testName
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    id<STWService> service = [[STWServiceLocale alloc] init];
+
+    XCTAssertEqual(@"locale", [service name], "the service name is locale");
+}
+
+- (void)testIsBackgroundJob
+{
+    id<STWService> service = [[STWServiceLocale alloc] init];
+
+    XCTAssert([service isBackgroundJob:@"getLanguage"], "getLanguage service method is a background job");
+}
+
+- (void)testGetLanguage
+{
+    STWServiceLocale *service = [[STWServiceLocale alloc] init];
+
+    id<STWServiceCallContext> serviceCallContext = mockProtocol(@protocol(STWServiceCallContext));
+
+    [service getLanguage:@{} withContext:serviceCallContext];
+
+    [verifyCount(serviceCallContext, times(1)) succeedWithString:[[NSLocale preferredLanguages] objectAtIndex:0]];
 }
 
 @end
