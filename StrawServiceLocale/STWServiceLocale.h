0a244fc86cd2b8b862a6afd8c2d29e7047cd5c0e
Yoshiya Hinosawa <stibium121@gmail.com>
2014-09-21 19:45:55 +0100

    Add documentation

--- StrawServiceLocale/STWServiceLocale.h
+++ StrawServiceLocale/STWServiceLocale.h
@@ -1,8 +1,19 @@
 #import <Foundation/Foundation.h>
 #import <Straw/Straw.h>
 
+/**
+ STWServiceLocale is a Locale service for Straw iOS.
+
+ This service class provides the way to access to locale data through Straw Service Method call.
+ */
 @interface STWServiceLocale : NSObject <STWService>
 
+/**
+ Returns language code.
+
+ @param params Straw Service call params
+ @param context Straw Service call context
+ */
 - (void)getLanguage:(NSDictionary *)params withContext:(id<STWServiceCallContext>)context;
 
 @end
